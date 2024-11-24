b574bca742a4a59c10d28f535e661cd5f707cab7
LewisClifton <lewisclifton2003@gmail.com>
2024-11-24 13:58:41 +0000

    Fixed downsample function

--- utils/degradation.py
+++ utils/degradation.py
@@ -1,6 +1,6 @@
 import numpy as np
 from utils.common import *
-import cv2
+from PIL import Image
 
 def add_gaussian_noise(image, std=1):
     image = np.clip(image + np.random.normal(scale=std, size=image.shape), 0, 255).astype(np.uint8)
@@ -14,6 +14,5 @@ def add_salt_pepper_noise(image, s=0.01, p=0.01):
     image[pepper] = 0
     return image
 
-def downsample(image, factor=2, interpolation=cv2.INTER_CUBIC):
-    new_size = (image.shape[1] // factor, image.shape[0] // factor)
-    return cv2.resize(image, new_size, interpolation=interpolation)
+def downsample(image, factor=2, interpolation=Image.BICUBIC):
+    return image.resize((image.width // factor, image.height // factor), resample=interpolation)
