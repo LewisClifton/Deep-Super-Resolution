fixed normalising

models/GAN/generator.py
@@ -58,7 +58,7 @@ class Generator(nn.Module):
 
         self.conv3 = nn.Conv2d(in_channels=64, out_channels=3 , kernel_size=9, stride=1, padding=4)
 
-        self.out = nn.Sigmoid()
+        self.out = nn.Tanh()
 
     def forward(self, x):
 

train_GAN.py
@@ -30,7 +30,7 @@ def GAN_ISR_train(gan_G, gan_D, lr, train_loader, num_epoch, train_log_freq, dev
     # Get metrics
     psnr = PSNR().to(device)
     ssim = SSIM(data_range=1.0).to(device)
-    lpips = LPIPS(net_type='alex', normalize=True).to(device)
+    lpips = LPIPS(net_type='alex').to(device)
     
     # Get optimisers for both models
     optim_G = torch.optim.Adam(gan_G.parameters(), lr=lr)