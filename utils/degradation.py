4d6d9becfc82cc0afee815b57d31309e2caf1eae
LewisClifton <lewisclifton2003@gmail.com>
2024-11-25 15:29:23 +0000

    fixed adding noise

--- utils/degradation.py
+++ utils/degradation.py
@@ -3,10 +3,12 @@ from utils.common import *
 from PIL import Image
 
 def add_gaussian_noise(image, std=1):
-    image = np.clip(image + np.random.normal(scale=std, size=image.shape), 0, 255).astype(np.uint8)
+    image = np.clip(image + np.random.normal(scale=std*255, size=image.shape), 0, 255).astype(np.uint8)
     return image
 
 def add_salt_pepper_noise(image, s=0.01, p=0.01):
+    s *= 255
+    p *= 255
     
     salt = np.random.rand(image.shape[0], image.shape[1], image.shape[2]) < s
     pepper = np.random.rand(image.shape[0], image.shape[1], image.shape[2]) < p
