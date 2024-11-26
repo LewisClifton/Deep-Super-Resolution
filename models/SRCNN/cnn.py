1ef4063de72feb8035fc49b1a298e030d4089805
LewisClifton <lewisclifton2003@gmail.com>
2024-11-26 00:20:40 +0000

    fixed padding

--- models/SRCNN/cnn.py
+++ models/SRCNN/cnn.py
@@ -6,9 +6,9 @@ class CNN(nn.Module):
 
         self.scale_factor = scale_factor
 
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=9, stride=1, padding=0)
-        self.conv2 = nn.Conv2d(in_channels=64, out_channels=32, kernel_size=5, stride=1, padding=0)
-        self.conv3 = nn.Conv2d(in_channels=32,out_channels=1, kernel_size=5, stride=1, padding=0)
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=9, stride=1, padding=4)
+        self.conv2 = nn.Conv2d(in_channels=64, out_channels=32, kernel_size=5, stride=1, padding=2)
+        self.conv3 = nn.Conv2d(in_channels=32,out_channels=3, kernel_size=5, stride=1, padding=2)
         self.relu = nn.ReLU(inplace=True)
 
     def forward(self, x):
